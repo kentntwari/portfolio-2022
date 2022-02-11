73f6b17dd167ce1cb719a9f3f609b86829bf2272
Kent Ntwari <designerjehovah@gmail.com>
2022-02-11 13:10:36 -0500

    configured app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import Menu from './components/menu';
+import Home from './routes/home';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Menu />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
