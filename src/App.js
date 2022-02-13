17f483dcee129397f1007e1d882a9be84d3a73f1
Kent Ntwari <designerjehovah@gmail.com>
2022-02-12 19:52:03 -0500

    routes added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import Menu from './components/menu';
 import Home from './routes/home';
+import About from './routes/about';
+import Works from './routes/works';
+import Website from './routes/works/Website';
+import Footer from './components/footer';
+
+import CircleTopLeft from './components/background/app/CircleTopLeft';
+import CircleBottomRight from './components/background/app/CircleBottomRight';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App relative">
       <Menu />
+
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/works" element={<Works />}>
+          <Route path=":website" element={<Website />} />
+        </Route>
       </Routes>
+
+      <Footer />
+
+      <Fragment>
+        <CircleTopLeft />
+        <CircleBottomRight />
+      </Fragment>
     </div>
   );
 }
