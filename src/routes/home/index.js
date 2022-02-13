134e4fc764c819f19db173129216811e4a4c3a82
Kent Ntwari <designerjehovah@gmail.com>
2022-02-12 19:53:04 -0500

    configure homepage

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 import Hero from './Hero';
+import Tech from './Tech';
+import Works from './Works';
 
 const Home = () => {
   return (
-    <main className="flex flex-col gap-[8.25rem]">
+    <main className="flex flex-col gap-33">
       <Hero />
+      <Tech />
+      <Works />
     </main>
   );
 };
