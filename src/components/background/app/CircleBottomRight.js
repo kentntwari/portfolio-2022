f88b579927ca84e580e3616a2ba15cdfdc3f5b18
Kent Ntwari <designerjehovah@gmail.com>
2022-02-12 19:44:12 -0500

    background elements and decorators

---
+++ src/components/background/app/CircleBottomRight.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CircleBottomRight = () => {
+  return (
+    <div
+      className="bg-green-14 absolute bottom-0 right-0 w-[384px] h-[384px]
+       rounded-full blur-3xl transform translate-x-[174px] translate-y-[130px]"></div>
+  );
+};
+
+export default CircleBottomRight;
---
+++ src/components/background/app/CircleTopLeft.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CircleTopLeft = () => {
+  return (
+    <div
+      className="bg-blue-13 absolute top-0 left-0 w-[384px] h-[384px]
+       rounded-full blur-[140px] transform -translate-x-[174px] -translate-y-[148px]"></div>
+  );
+};
+
+export default CircleTopLeft;
