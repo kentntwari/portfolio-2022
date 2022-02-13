f227ad24345c238f001502a2f4853864e9789c0c
Kent Ntwari <designerjehovah@gmail.com>
2022-02-12 19:49:57 -0500

    configuration of navigation component

--- src/components/Menu/Navigation.js
+++ src/components/Menu/Navigation.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useCallback } from 'react';
+import { Link } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
 
-const Navigation = () => {
+const Navigation = ({ hiddenOnMobile, className, applyClassOnLink }) => {
+  const displayLinks = useCallback(() => {
+    const links = ['Home', 'About', 'Works'];
+
+    return links.map((link) => (
+      <Link
+        key={uuidv4()}
+        to={link === 'Home' ? '/' : `/${link.toLowerCase()}`}
+        className={applyClassOnLink ? applyClassOnLink : ''}>
+        {link}
+      </Link>
+    ));
+  }, [applyClassOnLink]);
+
   return (
-    <div>Navigation</div>
-  )
-}
+    <nav className={`${hiddenOnMobile ? 'hidden' : className}`}>{displayLinks()}</nav>
+  );
+};
 
-export default Navigation
+export default Navigation;
