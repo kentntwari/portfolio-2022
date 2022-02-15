08e83b1874ec332c29fbda76aa3a1c2c7f05a4c5
Kent Ntwari <designerjehovah@gmail.com>
2022-02-15 16:02:51 -0500

    conditional styles added

--- src/components/background/app/CircleBottomRight.js
+++ src/components/background/app/CircleBottomRight.js
@@ -3,8 +3,8 @@ import React from 'react';
 const CircleBottomRight = () => {
   return (
     <div
-      className="bg-green-14 absolute bottom-0 right-0 w-[384px] h-[384px]
-       rounded-full blur-3xl transform translate-x-[174px] translate-y-[130px]"></div>
+      className="bg-green-14 absolute bottom-0 -right-[65px] -z-10 w-[300px] h-[300px]
+    rounded-full blur-[80px]"></div>
   );
 };
 
