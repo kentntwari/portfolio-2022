3d7643f26080ecc88b55efa7a4c5d45ee5e3cab9
Kent Ntwari <designerjehovah@gmail.com>
2022-02-22 10:47:24 -0500

    custom website component

--- src/routes/works/Website.js
+++ src/routes/works/Website.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
-const Website = () => {
-  return <div>Website</div>;
+import ButtonSecondary from '../../components/buttons/ButtonSecondary';
+
+const Website = ({ title, description }) => {
+  return (
+    <article className="bg-white-full p-[20px] flex flex-col gap-[20px] shadow-[0_4px_4px_rgba(196,196,196,0.25)]">
+      <h4 className="text-center">{title}</h4>
+      <p className="text-center">{description}</p>
+      <ButtonSecondary removeClasses={['px-10']} addClasses={['px-[40px]', 'm-auto']}>
+        Learn More
+      </ButtonSecondary>
+    </article>
+  );
 };
 
 export default Website;
