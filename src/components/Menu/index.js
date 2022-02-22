86a3c48e025a3f62fe952000bda325d8578e7162
Kent Ntwari <designerjehovah@gmail.com>
2022-02-22 10:34:01 -0500

    display menu functionality depending on context update

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useRef, createRef, useContext } from 'react';
+
+import blurContext from '../../global/context/blurContext';
 
 import Burger from '../../utilities/images/hamburger.png';
 import Navigation from './Navigation';
+import BlobMenuTopLeft from '../background/menu/BlobMenuTopLeft';
 
 const Menu = () => {
+  const context = useContext(blurContext);
+  const { hidden, triggerBgBlur } = context;
+
+  const burger_ref = useRef();
+  const blob_menu_ref = createRef();
+
   return (
-    <header className="relative mb-33">
-      <Navigation hiddenOnMobile={true} />
-      <img className="w-[50px] h-[50px]" src={Burger} alt="burger-nav" />
+    <header className="relative mb-33 z-20">
+      <Navigation hiddenOnMobile={hidden} />
+      <img
+        ref={burger_ref}
+        className={hidden ? 'w-[50px] h-[50px]' : 'hidden'}
+        src={Burger}
+        onClick={() => {
+          burger_ref.current.classList.add('hidden');
+
+          triggerBgBlur();
+
+          document.body.classList.add('overflow-hidden');
+        }}
+        alt="burger-nav-icon"
+      />
+
+      <BlobMenuTopLeft ref={blob_menu_ref} hiddenOnMobile={hidden} />
     </header>
   );
 };
