0ebfd43470d1e210bef5a02dca6f9c61af056628
Kent Ntwari <designerjehovah@gmail.com>
2022-02-22 10:26:08 -0500

    wrapped App in blur context

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 
+import BlurState from './global/context/BlurState';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <BlurState>
+        <App />
+      </BlurState>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
