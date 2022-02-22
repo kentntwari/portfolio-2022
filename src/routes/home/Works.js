a1b3043b5765032c9ff9bd7009286a5212a4fe54
Kent Ntwari <designerjehovah@gmail.com>
2022-02-22 10:44:22 -0500

    refactored with custom website component

--- src/routes/home/Works.js
+++ src/routes/home/Works.js
@@ -2,7 +2,7 @@ import React, { Fragment } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 import useFetchWebsites from '../../utilities/hooks/useFetchWebsites';
-import ButtonSecondary from '../../components/buttons/ButtonSecondary';
+import Website from '../works/Website';
 
 import CircleCenter from '../../components/background/homepage/CircleCenter';
 import BlobWorksHomeCenter from '../../components/background/homepage/BlobWorksHomeCenter';
@@ -16,13 +16,9 @@ const Works = () => {
       <div className="grid grid-cols-1 gap-[1.875rem]">
         {works !== null &&
           works.map(({ title, trailer }) => (
-            <article
-              key={uuidv4()}
-              className="bg-white-full p-5 shadow-md flex flex-col items-center gap-5">
-              <h4 className="text-center">{title}</h4>
-              <p className="text-center">{trailer}</p>
-              <ButtonSecondary>Learn More</ButtonSecondary>
-            </article>
+            <Fragment key={uuidv4()}>
+              <Website title={title} description={trailer} />
+            </Fragment>
           ))}
       </div>
 
