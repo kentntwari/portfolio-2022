572ee930ac9acc97fe21aa97c67acda1d803c6bb
Kent Ntwari <designerjehovah@gmail.com>
2022-02-22 10:24:14 -0500

    wrapped blur bg around app on menu reveal

--- src/App.js
+++ src/App.js
@@ -10,21 +10,26 @@ import Footer from './components/footer';
 
 import CircleTopLeft from './components/background/app/CircleTopLeft';
 import CircleBottomRight from './components/background/app/CircleBottomRight';
+import BlurredBg from './components/background/app/BlurredBg';
 
 function App() {
   return (
     <div className="App w-full px-[30px] py-16 relative overflow-x-hidden">
-      <Menu />
+      <Fragment>
+        <Menu />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/works" element={<Works />}>
-          <Route path=":website" element={<Website />} />
-        </Route>
-      </Routes>
-
-      <Footer />
+        <BlurredBg>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/works" element={<Works />}>
+              <Route path=":website" element={<Website />} />
+            </Route>
+          </Routes>
+        </BlurredBg>
+        
+        <Footer />
+      </Fragment>
 
       <Fragment>
         <CircleTopLeft />
