fb61a6482224617175c955bd9a1a4d24371d3bd8
Kent Ntwari <designerjehovah@gmail.com>
2022-02-22 10:44:51 -0500

    hero component created

---
+++ src/routes/works/Hero.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <section className="flex flex-col gap-[28px]">
+      <h1 className="text-center">Take a look at my work</h1>
+      <p className="text-center">
+        Here's a showcase of projects and concepts I've worked on. Who knows? Yours could
+        make the list one day
+      </p>
+    </section>
+  );
+};
+
+export default Hero;
