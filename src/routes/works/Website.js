c09ea0be45064fc5ac4ee031bf3635322274a37b
Kent Ntwari <designerjehovah@gmail.com>
2022-03-04 01:19:29 -0500

    refactored component for conciseness

--- src/routes/works/Website.js
+++ src/routes/works/Website.js
@@ -1,15 +1,20 @@
 import React from 'react';
 
+import { setButtonUrl as data } from '../../utilities/functions/setButtonUrl';
+
 import ButtonSecondary from '../../components/buttons/ButtonSecondary';
 
-const Website = ({ title, description }) => {
+const Website = ({ title, description, slug, bgColor }) => {
   return (
-    <article className="bg-white-full p-[20px] flex flex-col gap-[20px] shadow-[0_4px_4px_rgba(196,196,196,0.25)]">
-      <h4 className="text-center">{title}</h4>
-      <p className="text-center">{description}</p>
-      <ButtonSecondary removeClasses={['px-10']} addClasses={['px-[40px]', 'm-auto']}>
-        Learn More
-      </ButtonSecondary>
+    <article
+      className={`${bgColor} p-[20px] flex flex-col gap-10 
+      shadow-[0_4px_4px_rgba(196,196,196,0.25)] rounded-[10px]`}>
+      <div className="flex flex-col items-center gap-5">
+        <h4 className="text-center">{title}</h4>
+        <p className="text-center">{description}</p>
+      </div>
+
+      <ButtonSecondary {...data(slug)}>Learn More</ButtonSecondary>
     </article>
   );
 };
