11e7e572ba35851563c6d474928321696d4dff19
Kent Ntwari <designerjehovah@gmail.com>
2022-03-04 01:15:19 -0500

    stykes added

--- src/routes/works/Hero.js
+++ src/routes/works/Hero.js
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <section className="flex flex-col gap-[28px]">
       <h1 className="text-center">Take a look at my work</h1>
-      <p className="text-center">
+      <p className="text-center w-[283px] m-auto">
         Here's a showcase of projects and concepts I've worked on. Who knows? Yours could
         make the list one day
       </p>
