a4603dd240b408ad4c9b9a4d6028c651d2600f77
Kent Ntwari <designerjehovah@gmail.com>
2022-03-04 01:30:05 -0500

    refactored component for conciseness

--- src/routes/home/Works.js
+++ src/routes/home/Works.js
@@ -2,8 +2,9 @@ import React, { Fragment } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 import useFetchWebsites from '../../utilities/hooks/useFetchWebsites';
-import Website from '../works/Website';
+import { websiteDataProps as data } from '../../utilities/functions/websiteDataProps';
 
+import Website from '../works/Website';
 import CircleCenter from '../../components/background/homepage/CircleCenter';
 import BlobWorksHomeCenter from '../../components/background/homepage/BlobWorksHomeCenter';
 
@@ -11,13 +12,13 @@ const Works = () => {
   const works = useFetchWebsites();
 
   return (
-    <article className="relative z-10 flex flex-col items-center gap-14">
+    <article className="relative z-10 flex flex-col items-center gap-[42px]">
       <h2 className="text-center">Explore more of my work</h2>
-      <div className="grid grid-cols-1 gap-[1.875rem]">
+      <div className="grid grid-cols-1 gap-[20px]">
         {works !== null &&
-          works.map(({ title, trailer }) => (
+          works.map(({ title, trailer, slug }, index) => (
             <Fragment key={uuidv4()}>
-              <Website title={title} description={trailer} />
+              <Website {...data(index, title, trailer, slug)} />
             </Fragment>
           ))}
       </div>
@@ -26,7 +27,7 @@ const Works = () => {
         <CircleCenter
           bgColor="bg-pink-16"
           coordinatesX="left-0"
-          coordinatesY="top-[450px]"
+          coordinatesY="top-[270px]"
         />
         <BlobWorksHomeCenter />
       </Fragment>
