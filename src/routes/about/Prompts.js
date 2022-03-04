a33d3f6e0f0306b624e791ce8072f70d5b9b8a59
Kent Ntwari <designerjehovah@gmail.com>
2022-03-04 01:32:06 -0500

    refactored component

--- src/routes/about/Prompts.js
+++ src/routes/about/Prompts.js
@@ -1,20 +1,16 @@
-import React, { useCallback } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Fragment, useCallback } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+
+import ButtonPrimary from '../../components/buttons/ButtonPrimary';
 
 const Prompts = () => {
   const contactLinks = useCallback(() => {
     const links = ['Indeed', 'Linkedin'];
 
     return links.map((link) => (
-      <Link
-        key={uuidv4()}
-        to="/"
-        className="bg-pink-100 py-[10px] px-[40px]
-        shadow-[0_4px_4px_rgba(196,196,196,0.25)] rounded-3xl
-        text-white-full text-center font-display">
-        Contact me on {link}
-      </Link>
+      <Fragment key={uuidv4()}>
+        <ButtonPrimary url="/">Contact me on {link}</ButtonPrimary>
+      </Fragment>
     ));
   }, []);
 
