fd0398567f2ebd9c0caaf95b682dd9a72c483898
Kent Ntwari <designerjehovah@gmail.com>
2022-03-12 18:23:28 -0500

    component updated

--- src/routes/works/Hero.js
+++ src/routes/works/Hero.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Hero = () => {
   return (
-    <section className="flex flex-col gap-[28px]">
-      <h1 className="text-center">Take a look at my work</h1>
-      <p className="text-center w-[283px] m-auto">
+    <section className="flex flex-col lg:items-center gap-5">
+      <h1 className="md:text-center">Take a look at my work</h1>
+      <p className="w-11/12 lg:text-center lg:text-[21.33px]">
         Here's a showcase of projects and concepts I've worked on. Who knows? Yours could
         make the list one day
       </p>
--- src/routes/works/Websites.js
+++ src/routes/works/Websites.js
@@ -1,28 +1,13 @@
 import React, { Fragment } from 'react';
-import { v4 as uuidv4 } from 'uuid';
 
-import useFetchWebsites from '../../utilities/hooks/useFetchWebsites';
-import { websiteDataProps as data } from '../../utilities/functions/websiteDataProps';
-
-import Website from './Website';
+import WebsitesBlock from '../../components/blocks/WebsitesBlock';
 import BlobWebsitesWorksRight from '../../components/background/workspage/BlobWebsitesWorksRight';
 
 const Websites = () => {
-  const websites = useFetchWebsites();
-
   return (
-    <main className="relative mt-12 flex flex-col gap-5">
-      {websites !== null ? (
-        websites.map(({ title, trailer, slug }, index) => (
-          <Fragment key={uuidv4()}>
-            <Website {...data(index, title, trailer, slug)} />
-          </Fragment>
-        ))
-      ) : (
-        <div className="w-full">
-          <h2 className="text-center">Loading</h2>
-        </div>
-      )}
+    <main
+      className="relative mt-14">
+      <WebsitesBlock />
 
       <Fragment>
         <BlobWebsitesWorksRight />
