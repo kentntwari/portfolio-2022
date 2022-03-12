2994912b3c6eaf8d5372862e74da8ddec1a37ba8
Kent Ntwari <designerjehovah@gmail.com>
2022-03-12 18:24:48 -0500

    function refactored

--- src/utilities/functions/websiteDataProps.js
+++ src/utilities/functions/websiteDataProps.js
@@ -1,9 +1,5 @@
+import { generateBgColor as bg } from './generateBgColor';
+
 export function websiteDataProps(index, title, trailer, slug) {
-  function evaluateBg(number) {
-    if (number % 2 === 0) return 'bg-white-full';
-
-    if (number % 2 !== 0) return 'bg-white-site';
-  }
-
-  return { bgColor: evaluateBg(index), title, description: trailer, slug };
+  return { bgColor: bg(index), title, description: trailer, slug };
 }
