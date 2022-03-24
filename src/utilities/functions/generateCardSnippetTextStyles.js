8c7320906e7b5d2ef8921f1bc0900e446dac8133
Kent Ntwari <designerjehovah@gmail.com>
2022-03-24 06:09:27 -0400

    helper function for demo card dynamic individual classes

--- src/utilities/functions/generateCardSnippetTextStyles.js
+++ src/utilities/functions/generateCardSnippetTextStyles.js
@@ -1,5 +1,22 @@
+import { createClassString } from './createClassString';
+
+const code_styles = createClassString({
+  top: '-top-36 lg:-top-52 2xl:-top-[12.5rem]',
+  left: '-left-32 xl:-left-72 2xl:-left-48',
+});
+
+const figma_styles = createClassString({
+  top: '-top-[8.25rem] 2xl:-top-44',
+  left: '-left-[20.5rem]',
+});
+
+const website_styles = createClassString({
+  top: 'lg:top-24 2xl:top-[11.5rem]',
+  left: '-left-[20.5rem] 2xl:-left-[25.5rem]',
+});
+
 export function cardTextClass(title) {
-  if (title === 'code') return '-top-36 -left-32';
-  if (title === 'figma') return '-top-[8.25rem] -left-[20.5rem]';
-  if (title === 'website') return 'top-[5.5rem] -left-[20.5rem]';
+  if (title === 'code') return code_styles;
+  if (title === 'figma') return figma_styles;
+  if (title === 'website') return website_styles;
 }
