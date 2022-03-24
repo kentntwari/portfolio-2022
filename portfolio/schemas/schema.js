dd0b16ca77a336eaf0743b309700033104deff00
Kent Ntwari <designerjehovah@gmail.com>
2022-03-24 06:26:59 -0400

    added demo info to be fetched from sanity

--- portfolio/schemas/schema.js
+++ portfolio/schemas/schema.js
@@ -9,6 +9,7 @@ import blockContent from './blockContent';
 import about from './about';
 import technologies from './technologies';
 import website from './website';
+import Demo from './homeDemo';
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -25,5 +26,6 @@ export default createSchema({
     // When added to this list, object types can be used as
     // { type: 'typename' } in other document schemas
     blockContent,
+    Demo,
   ]),
 });
