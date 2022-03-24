41d2ec591026b58ae428b3bf23943c92af28bbe0
Kent Ntwari <designerjehovah@gmail.com>
2022-03-24 04:41:12 -0400

    renamed files
    prefixed with api for better traceability

--- src/api/api.website.js
+++ src/api/api.website.js
@@ -2,6 +2,7 @@ export function website(slug) {
   return `*[_type=='website' && slug.current=='${slug}'][0]{
     'challenges':challenges[0].children[0].text,
     features,
+    'preview': preview.asset->url,
     summary,
     title,
     trailer
